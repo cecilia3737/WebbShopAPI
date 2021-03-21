c4c18a0a98ba042e2260a55bf931438630092fe1
Cecilia Nilsson <cecilia.o.nilsson@live.se>
2021-03-21 15:40:56 +0100

    Add project files.

---
+++ WebbShopAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebbShopAPI.Database;
+using WebbShopAPI.Helpers;
+using WebbShopAPI.Views;
+
+namespace WebbShopAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new WScontext())
+            {
+                var test = new TestDemo();
+
+                test.Demo();
+
+            }
+
+        }
+    }
+}
